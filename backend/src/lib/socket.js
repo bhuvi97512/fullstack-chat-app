f36de44bec2122d3159db286271351b9aacd395f
bhuvi97512 <bhuvi97512@gmail.com>
2025-09-10 17:05:57 +0530

    Project Completed

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -8,7 +8,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: ["*"],
+        origin: ["https://fullstack-chat-app-rust.vercel.app"],
     },
 });
 
