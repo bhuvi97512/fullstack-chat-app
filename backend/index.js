915254d5dce94a62a76dedacfbf35da0a1b30d85
bhuvi97512 <bhuvi97512@gmail.com>
2025-09-10 17:04:52 +0530

    Project Completed

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
     cors({
-        origin: "*",
+        origin: "https://fullstack-chat-app-rust.vercel.app",
         credentials: true,
     })
 );
